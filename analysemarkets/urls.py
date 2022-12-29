ac533b706eb9a8a6105a8dcfac666873be650a9b
knowledgecomputer2018 <61306250+knowledgecomputer2018@users.noreply.github.com>
2022-12-29 18:25:01 +0330

    f2
    upload

---
+++ analysemarkets/urls.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""analysemarkets URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from payment import urls as cryptocurrency_payment_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('',include('analyseData.urls')),
+    path('analyseData/',include('analyseData.urls')),
+    path('blog/',include('blog.urls')),
+
+    path('payment/',include('payment.urls')),
+    path(r'^', include(cryptocurrency_payment_urls)), #/payment/{pk}/
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
